ec54ad225ae34289b2dc9513436b8900aa212195
AKSHASHA <aakankshah18@gmail.com>
2020-05-30 17:00:40 +0530

    Covid_19 stats Technology Stack:Json,Api,HttpURLConnection

---
+++ app/src/main/java/com/example/covid_19statisticscountrywise/MainActivity.java
@@ -0,0 +1,158 @@
+package com.example.covid_19statisticscountrywise;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static android.widget.Toast.LENGTH_LONG;
+
+public class MainActivity extends AppCompatActivity {
+
+
+ArrayAdapter<String> ad;
+ArrayList<String> ar = new ArrayList<String>();
+AutoCompleteTextView actv;
+  public class DownloadTask extends AsyncTask<String,Void,String> {
+
+      @Override
+      protected String doInBackground(String... strings) {
+          String res = "";
+          URL url;
+          HttpURLConnection urlConnection = null;
+          try {
+              url = new URL(strings[0]);
+              urlConnection = (HttpURLConnection) url.openConnection();
+              InputStream ior = urlConnection.getInputStream();
+              InputStreamReader reader = new InputStreamReader(ior);
+              int data = reader.read();
+              while (data != -1) {
+                  char current = (char) data;
+                  res += current;
+                  data = reader.read();
+              }
+              return res;
+
+
+          } catch (MalformedURLException e) {
+              e.printStackTrace();
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+
+          return "";
+      }
+
+      @Override
+      protected void onPostExecute(String res) {
+          super.onPostExecute(res);
+                    try {
+             JSONObject jsonObj = new JSONObject(res);
+              JSONArray ja_data = jsonObj.getJSONArray("Countries");
+              int length = ja_data.length();
+              for (int i = 0; i < length; i++) {
+                  JSONObject jObj = ja_data.getJSONObject(i);
+                 //Log.i("conyty list:", jObj.getString("Country"));
+                ar.add(jObj.getString("Country"));
+                //ad = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,ar);
+                //actv.setAdapter(ad);
+//                  for(int ij = 0; ij < ar.size(); ij++) {
+//                     Log.i("Arraylist",ar.get(ij));
+//                  }
+              }  ad = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,ar);
+                 actv.setAdapter(ad);
+                        //for(int ij = 0; ij < ar.size(); ij++) {
+                      //Log.i("Arraylist",ar.get(ij));
+                  //}
+         } catch (JSONException e) {
+              e.printStackTrace();
+        }
+//
+
+      }
+
+  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+       actv=(AutoCompleteTextView)findViewById(R.id.autoCompleteTextView);
+        DownloadTask task=new DownloadTask();
+        task.execute("https://api.covid19api.com/summary");
+//        for(int ij = 0; ij < ar.size(); ij++)
+//        {
+//                     Log.i("Arraylist",ar.get(ij));
+//        }
+        //ad = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,ar);
+        //actv.setAdapter(ad);
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+   public void nextClicked(View view)
+   {String pass=actv.getText().toString();
+       Intent intentz=new Intent(this,Stats.class);
+        intentz.putExtra("epuzzle",pass);
+        startActivity(intentz);
+
+    }
+}
+
+
+
+
+
---
+++ app/src/main/java/com/example/covid_19statisticscountrywise/Stats.java
@@ -0,0 +1,136 @@
+package com.example.covid_19statisticscountrywise;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class Stats extends AppCompatActivity {
+
+    EditText country,tc,tnc,tr,tnr,td,tnd;
+    ArrayList<String> countryName=new ArrayList<String>();
+    ArrayList<String> tcases=new ArrayList<String>();
+    ArrayList<String> tnewcases=new ArrayList<String>();
+    ArrayList<String> trec=new ArrayList<String>();
+    ArrayList<String> tnewrec=new ArrayList<String>();
+    ArrayList<String> tdeath=new ArrayList<String>();
+    ArrayList<String> tnewdeath=new ArrayList<String>();
+    String easyPuzzle;
+
+
+
+    public class DownloadTaskk extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String res = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream ior = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(ior);
+                int data = reader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    res += current;
+                    data = reader.read();
+                }
+                return res;
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String res) {
+            super.onPostExecute(res);
+
+            try {
+                JSONObject jsonObjs = new JSONObject(res);
+                JSONArray ja_data = jsonObjs.getJSONArray("Countries");
+                int length = ja_data.length();
+                for (int i = 0; i < length; i++) {
+                    JSONObject jObjs = ja_data.getJSONObject(i);
+                    countryName.add(jObjs.getString("Country"));
+                    tcases.add(jObjs.getString("TotalConfirmed"));
+                    tnewcases.add(jObjs.getString("NewConfirmed"));
+                    tnewdeath.add(jObjs.getString("NewDeaths"));
+                    tdeath.add(jObjs.getString("TotalDeaths"));
+                    tnewrec.add(jObjs.getString("NewRecovered"));
+                    trec.add(jObjs.getString("TotalRecovered"));
+
+                }
+//                for(int ij = 0; ij < tnewcases.size(); ij++) {
+//                    Log.i("Arraylist",tnewcases.get(ij));
+//                    }
+                int position=countryName.indexOf(easyPuzzle);
+                country.setText(easyPuzzle);
+                tc.setText(tcases.get(position));
+                tnc.setText(tnewcases.get(position));
+                tr.setText(trec.get(position));
+                tnr.setText(tnewrec.get(position));
+                td.setText(tdeath.get(position));
+                tnd.setText(tnewdeath.get(position));
+
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        country=findViewById(R.id.conans);
+        tc=findViewById(R.id.conans2);
+        tnc=findViewById(R.id.conans3);
+        tr=findViewById(R.id.conans4);
+        tnr=findViewById(R.id.conans5);
+        td=findViewById(R.id.conans6);
+        tnd=findViewById(R.id.conans7);
+        Intent intent = getIntent();
+        easyPuzzle = intent.getExtras().getString("epuzzle");
+
+        DownloadTaskk tk=new DownloadTaskk();
+        tk.execute("https://api.covid19api.com/summary");
+
+
+    }
+
+}
